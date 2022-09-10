df3f25131bc5ccdd4c617fd2759dfea11862c034
Rishav Kumar <20bds045@iiitdwd.ac.in>
2022-09-10 18:28:37 +0530

    modifying the programme add_two_numbers

--- add_two_numbers.cpp
+++ add_two_numbers.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main()
 {
-    int a ,b;
+    int a ,b,c;
+    c = a+b;
     cin>>a>>b;
-    cout<<"The sum of two numbers = "<<a+b<<endl;
+    cout<<"The sum of two numbers = "<<c<<endl;
 }
